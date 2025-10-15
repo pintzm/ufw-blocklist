e1452bdc4be1c6a9e558ce9a3a31946df9010767
pintzm <maxi.pintz@yahoo.de>
2025-10-15 12:54:28 +0200

    Added IPv6 allowlist

--- install-blocklist.sh
+++ install-blocklist.sh
@@ -1,21 +1,37 @@
+#!/bin/bash
+set -Eeuo pipefail
 
+sudo mkdir -p /etc/ufw/backups
 if [[ -f /etc/ufw/blocklist.txt ]]; then
-    sudo mv /etc/ufw/blocklist.txt /etc/ufw/blocklist.txt.bak
-    echo "Backup of existing blocklist.txt stored to /etc/ufw/blocklist.txt.bak"
+    bakfile="$(mktemp -p /etc/ufw/backups --suffix=.bak -u blocklist.txtXXXXX)"
+    sudo mv /etc/ufw/blocklist.txt $bakfile
+    echo "Backup of existing blocklist.txt stored to $bakfile"
 fi
 if [[ -f /etc/ufw/blocklist-allow.txt ]]; then
-    sudo mv /etc/ufw/blocklist-allow.txt /etc/ufw/blocklist-allow.txt.bak
-    echo "Backup of existing blocklist-allow.txt stored to /etc/ufw/blocklist-allow.txt.bak"
+    bakfile="$(mktemp -p /etc/ufw/backups --suffix=.bak -u blocklist-allow.txtXXXXX)"
+    sudo mv /etc/ufw/blocklist-allow.txt $bakfile
+    echo "Backup of existing blocklist-allow.txt stored to $bakfile"
+fi
+if [[ -f /etc/ufw/blocklist6-allow.txt ]]; then
+    bakfile="$(mktemp -p /etc/ufw/backups --suffix=.bak -u blocklist6-allow.txtXXXXX)"
+    sudo mv /etc/ufw/blocklist6-allow.txt $bakfile
+    echo "Backup of existing blocklist6-allow.txt stored to $bakfile"
+fi
+if [[ -f /etc/ufw/after.init ]]; then
+    bakfile="$(mktemp -p /etc/ufw/backups --suffix=.bak -u after.initXXXXX)"
+    sudo mv /etc/ufw/after.init $bakfile
+    echo "Backup of existing after.init stored to $bakfile"
 fi
 sudo cp blocklist-allow.txt /etc/ufw/blocklist-allow.txt
 sudo chown root:root /etc/ufw/blocklist-allow.txt
+
+sudo cp blocklist6-allow.txt /etc/ufw/blocklist6-allow.txt
+sudo chown root:root /etc/ufw/blocklist6-allow.txt
 
 sudo cp update-blocklist-sources.sh /usr/local/sbin/update-blocklist-sources.sh
 sudo chown root:root /usr/local/sbin/update-blocklist-sources.sh
 sudo chmod +x /usr/local/sbin/update-blocklist-sources.sh
 
-sudo mv /etc/ufw/after.init /etc/ufw/after.init.bak
-echo "Backup of original after.init at /etc/ufw/after.init.bak"
 sudo cp after.init /etc/ufw/
 sudo chown root:root /etc/ufw/after.init
 sudo chmod +x /etc/ufw/after.init
@@ -26,5 +42,6 @@ sudo ufw status verbose
 # Inspect placement:
 sudo iptables -L ufw-before-input --line-numbers -v
 sudo iptables -L ufw-before-output --line-numbers -v
+sudo iptables -L ufw-before-forward --line-numbers -v
 sudo iptables -S BLOCKLIST
 sudo ipset list blocklist | head
